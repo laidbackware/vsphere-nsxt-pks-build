ab028ba1d1b6b52721b12a99275f14a5c64a33d6
Matt Proud <proudmatt@gmail.com>
2019-04-26 16:12:44 +0100

    update with latest build spec from optimize branch

--- build_vsphere.sh
+++ build_vsphere.sh
@@ -5,12 +5,13 @@ set -x # print commands
 
 git clone https://github.com/laidbackware/vsphere-lab-deploy.git
 
-rm ./vsphere-lab-deploy/*yml
-
-cp vsphere-answerfile.yml ./vsphere-lab-deploy/answerfile.yml
-cp vsphere-deploy.yml ./vsphere-lab-deploy/
-
 cd vsphere-lab-deploy
-sudo ansible-playbook vsphere-deploy.yml -vvv
+git checkout optimize_build
+
+rm ./*yml
+
+cp ../vsphere-answerfile.yml ./answerfile.yml
+cp ../vsphere-deploy.yml .
+ansible-playbook vsphere-deploy.yml
 
 cd ..
